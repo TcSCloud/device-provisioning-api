e7a59873aae85a5407c3cc9c104059e15717ab5f
TaiwoT <back4good@yahoo.com>
2025-02-11 19:20:36 +0000

    Updated app.py with API routes

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask, jsonify
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/device-status', methods=['GET'])
-def device_status():
-    return jsonify({"status": "Device is ready for provisioning", "code": 200})
+@app.route("/")
+def home():
+    return "API is running!"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+@app.route("/health")
+def health():
+    return "Health Check OK", 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
