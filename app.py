90fb43083d9108b1d10cce8f142c766d979f51d5
TaiwoT <back4good@yahoo.com>
2025-02-11 19:40:04 +0000

    Updated app.py with health check and added requirements

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-from flask import Flask, jsonify
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "API is running successfully!"
+    return "API is running successfully!", 200
 
-@app.route('/health', methods=['GET'])
+@app.route('/health')
 def health():
-    return jsonify({"status": "healthy"}), 200
+    return "Healthy", 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
